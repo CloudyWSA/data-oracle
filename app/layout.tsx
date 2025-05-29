ae45c434f5f06c2bd59f348a71d1a7001006a77c
unknown <matheusbaronicontato@gmail.com>
2025-05-29 12:33:30 -0300

    fix role suggestion algorithm, better ui and more

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata: Metadata = {
   title: "Data Oracle",
   description: "League of Legends Pro Data",
@@ -14,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body>{children}</body>
     </html>
+
   )
 }
